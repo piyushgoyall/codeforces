cc03f9a87e03fb9ec41988069ee00d7ca7aa96bc
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-01-20 09:46:13 +0530

    new question solved

---
+++ cpp/A. Marathon.c
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int count = 0;
+        if (a < b && a < c && a < d)
+        {
+            count = 3;
+        }
+        else if (a < b && a > c && a > d || a < c && a > b && a > d || a < d && a > c && a > b)
+        {
+            count = 1;
+        }
+        else if (a > b && a > c && a > d)
+        {
+            count = 0;
+        }
+        else
+        {
+            count = 2;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
